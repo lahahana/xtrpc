cf78a3fcd19eb1e0a48680852787b28e6e41b564
lahahana <dailebing666@gmail.com>
2018-01-15 23:09:03 +0800

    add http exposure function(idea) by undertow
    Signed-off-by: lahahana <dailebing666@gmail.com>

--- xtrpc-rpc/src/main/java/com/github/lahahana/xtrpc/server/http/ExportAsHttpService.java
+++ xtrpc-rpc/src/main/java/com/github/lahahana/xtrpc/server/http/ExportAsHttpService.java
@@ -7,5 +7,15 @@ public @interface ExportAsHttpService {
      */
     boolean exportAsHttpServiceOnly() default false;
 
+    /**
+     * the http port of http service
+     */
     int port() default 8081;
+
+    /**
+     * Default value will be the path of annotated interface's package name plus method name,
+     * "com.github.lahahana.demo.service.UserService.getUserById()" -> /com/github/lahahana/demo/service/UserService/getUserById
+     */
+    String path() default "";
+
 }
